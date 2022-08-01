a12025b70875e00249e38133f6128d1a31a19b4d
kev <kevHTW@gmx.com>
2022-08-01 18:01:37 +0200

    add Singleton example

---
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Singleton
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T _mInstance;
+
+        protected static T Instance
+        {
+            get
+            {
+                if (_mInstance != null) return _mInstance;
+                return FindObjectOfType(typeof(T)) as T;
+            }
+        }
+
+        protected static bool Exists()
+        {
+            return (_mInstance != null);
+        }
+
+        private void Awake()
+        {
+            if (_mInstance != null) return;
+            _mInstance = this as T;
+        }
+
+        private void OnApplicationQuit()
+        {
+            _mInstance = null;
+        }
+    }
+}
