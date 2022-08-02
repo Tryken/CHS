bf33e0b73ae6008f0f35c87caaf403c94433a4c9
kev <kevHTW@gmx.com>
2022-08-02 17:25:45 +0200

    update Singleton version

--- Assets/Scripts/Singleton/Singleton.cs
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -1,34 +1,14 @@
-﻿using UnityEngine;
-
-namespace Singleton
+﻿namespace Singleton
 {
-    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    public abstract class Singleton <T>  where T : Singleton<T>, new()
     {
         private static T _mInstance;
+        
+        protected static T Instance => _mInstance ??= new T();
 
-        protected static T Instance
-        {
-            get
-            {
-                if (_mInstance != null) return _mInstance;
-                return FindObjectOfType(typeof(T)) as T;
-            }
-        }
-
-        protected static bool Exists()
+        protected bool Exists()
         {
             return (_mInstance != null);
-        }
-
-        private void Awake()
-        {
-            if (_mInstance != null) return;
-            _mInstance = this as T;
-        }
-
-        private void OnApplicationQuit()
-        {
-            _mInstance = null;
         }
     }
 }
