49ef0a345613be9991d592a7a59517befb5ca9c8
Sven Ciesla <ciesla.sven@ahoo.de>
2022-08-02 19:23:52 +0200

    added initial gamemanager

---
+++ Assets/Scripts/GameManager/GameManagerAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MoonSharp.Interpreter.Interop;
+using Singleton;
+
+namespace GameManager
+{
+	public class GameManagerAPI : Singleton<GameManagerAPI> 
+	{
+		public event EventHandler OnInitGame;
+		public event EventHandler OnStartGame;
+		public event EventHandler OnRunGame;
+		public event EventHandler OnStopGame;
+		public event EventHandler OnCloseGame;
+		
+		[MoonSharpVisible(false)]
+		public void RaiseOnInitGame()
+		{
+			OnInitGame?.Invoke(this, EventArgs.Empty);
+		}
+		
+		[MoonSharpVisible(false)]
+		public void RaiseOnStartGame()
+		{
+			OnStartGame?.Invoke(this, EventArgs.Empty);
+		}
+		
+		[MoonSharpVisible(false)]
+		public void RaiseOnRunGame()
+		{
+			OnRunGame?.Invoke(this, EventArgs.Empty);
+		}
+		
+		[MoonSharpVisible(false)]
+		public void RaiseOnStopGame()
+		{
+			OnStopGame?.Invoke(this, EventArgs.Empty);
+		}
+		
+		[MoonSharpVisible(false)]
+		public void RaiseOnCloseGame()
+		{
+			OnCloseGame?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
