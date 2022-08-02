481788563325cecfb4564e4b25be9344fdcba495
kev <kevHTW@gmx.com>
2022-08-02 17:51:40 +0200

    Add EventSystem

---
+++ Assets/Scripts/Events/ExampleGUIEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events
+{
+    public class ExampleGUIEvent : MonoBehaviour
+    {
+        [SerializeField] private GameEvent _showGUI;
+
+        private void Update()
+        {
+            if (!Input.GetKey(KeyCode.Space)) return;
+            _showGUI?.Invoke();
+        }
+        
+    }
+}
