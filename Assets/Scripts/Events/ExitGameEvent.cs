74a8b5b67cd6ef6d8f6d1cf06e98c1a7d62ccd79
kev <kevHTW@gmx.com>
2022-08-03 23:43:58 +0200

    Move Exit to event system

---
+++ Assets/Scripts/Events/ExitGameEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Events
+{
+    public class ExitGameEvent : MonoBehaviour
+    {
+        [field: SerializeField] private GameEvent exitGame;
+        [field: SerializeField] private Button _button;
+
+        private void Start()
+        {
+            _button.onClick.AddListener(TaskOnClick);
+        }
+
+        private void TaskOnClick()
+        {
+            exitGame?.Invoke();
+        }
+    }
+}
