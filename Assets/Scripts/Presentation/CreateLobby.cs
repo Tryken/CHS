6c0e07a724c0f99b818e5addf76ad18c34f328ff
kev <kevHTW@gmx.com>
2022-08-04 23:53:58 +0200

    Add namespace

--- Assets/Scripts/Presentation/CreateLobby.cs
+++ Assets/Scripts/Presentation/CreateLobby.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateLobby : MonoBehaviour
+namespace Presentation
 {
-    [field: SerializeField] private RectTransform createLobbyMenu;
+    public class CreateLobby : MonoBehaviour
+    {
+        [field: SerializeField] private RectTransform createLobbyMenu;
 
-    private GameObject _mainMenu;
+        private GameObject _mainMenu;
         
-    private void Start()
-    {
-        _mainMenu = GameObject.FindWithTag("MainScreen");
-    }
+        private void Start()
+        {
+            _mainMenu = GameObject.FindWithTag("MainScreen");
+        }
     
-    public void CreateALobby()
-    {
-        _mainMenu.transform.gameObject.SetActive(false);
-        createLobbyMenu.transform.gameObject.SetActive(true);
+        public void CreateALobby()
+        {
+            _mainMenu.transform.gameObject.SetActive(false);
+            createLobbyMenu.transform.gameObject.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/Presentation/Exit.cs
+++ Assets/Scripts/Presentation/Exit.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Exit : MonoBehaviour
+namespace Presentation
 {
-    public void CloseGame()
+    public class Exit : MonoBehaviour
     {
+        public void CloseGame()
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
+        }
     }
 }
