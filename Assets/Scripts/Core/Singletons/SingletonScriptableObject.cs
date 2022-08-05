d83465409e74b9e7b1d324a77cec6ef411484725
kev <kevHTW@gmx.com>
2022-08-05 20:26:01 +0200

    Fix Stackoverflow

---
+++ Assets/Scripts/Core/Singletons/SingletonScriptableObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Core.Singletons
+{
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
+    {
+        private static T _mInstance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_mInstance != null) return _mInstance;
+                return FindObjectOfType(typeof(T)) as T;
+            }
+        }
+
+        protected bool Exists()
+        {
+            return (_mInstance != null);
+        }
+
+        private void Awake()
+        {
+            if (_mInstance != null) return;
+            _mInstance = this as T;
+        }
+
+        private void OnApplicationQuit()
+        {
+            _mInstance = null;
+        }
+    }
+}
