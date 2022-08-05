cdc0ea0833fbbb6085db27d4e5ea9214ea21f387
kev <kevHTW@gmx.com>
2022-08-05 16:22:22 +0200

    Add MasterManager as singleton

--- Assets/Scripts/Core/Networks/ScriptableObjects/MasterManager.cs
+++ Assets/Scripts/Core/Networks/ScriptableObjects/MasterManager.cs
@@ -1,10 +1,23 @@
+using System;
+using Core.Network.ScriptableObjects;
 using UnityEngine;
 
-namespace Core.Network.ScriptableObjects
+namespace Core.Networks.ScriptableObjects
 {
     [CreateAssetMenu(menuName = "Manager/MasterManager")]
     public class MasterManager : ScriptableObject
     {
-        public static GameSettings GameSettings => CreateInstance<GameSettings>();
+        public static GameSettings GameSettings
+        {
+            get
+            {
+                if (GameSettings is null)
+                {
+                    CreateInstance<GameSettings>();
+                }
+
+                return GameSettings;
+            }
+        }
     }
 }
